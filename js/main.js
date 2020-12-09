a42b4f07813e26b407982b6a70375e087cc8193a
manuelcastellanos <manuel.castellanos88@gmail.com>
2020-12-08 19:47:29 -0600

    primer codigo luna

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+console.log('Hola Koders')
+
+//variables
+
+//let, var, const
+var nombre variable;
+let, combreVariable2
+
+let nombreVariable = 'Algo'
+let numero = 1000
+let booleano = true
+let myArray = [1,'1', true]
+let myObjectKoder = {
+    nombre: 'manuel',
+}
+
+let nombre = 'oscar'
+nombre = "Oscar"
+
+//let si va a cambiar mi valor
+//const si es un valor fijo
+
+const NAME_KODER = "oscar"
+let name_koder2 = 'oscar'
+ 
+
+
---
+++ js/weight_in_moon.js
@@ -0,0 +1,7 @@
+let weight_moon = prompt ("¿Cuanto pesas en la luna?")
+
+console.log(weight_user)
+
+let moonWeight = parseInt (weight_user / 981) * 1622
+
+alert("En la luna pesas + moonWeight")
