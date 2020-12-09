5dc8ae984e644d3f7f63b6d721155988a872863b
manuelcastellanos <manuel.castellanos88@gmail.com>
2020-12-08 21:55:27 -0600

    numero mayor con ternario

---
+++ js/ternario.js
@@ -0,0 +1,14 @@
+// if ternario
+// expression? si true : si false
+// Comparar si el primer numero es mayor que el segundo
+// imprimir el primero
+// sino
+//imprimir el segundo
+
+const NUMBER_ONE = parseInt( prompt('Pon un numero we'))
+const NUMBER_TWO = parseInt( prompt('Ahora pon otro man'))
+
+NUMBER_ONE > NUMBER_TWO ? 
+    console.log (`El ${NUMBER_ONE} es el mayor`) 
+    : 
+    console.log (`El ${NUMBER_TWO} es el mayor`)
